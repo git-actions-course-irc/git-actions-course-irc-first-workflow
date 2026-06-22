ff31d2b5649dd14cbd89881bfc68f6cb14866f5d
Ishwar406 <chauhanishwar406@gmail.com>
2026-06-22 16:29:03 +0530

    commit

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,25 +1,20 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+try {
+  const name = core.getInput("who_to_greet");
 
-try{
-    core.debug('Debug Message');
-    core.warning('Warning Message');
-    core.error('Error Message');
+  console.log(`Hello ${name}`);
 
-    const name = core.getInput("who-to-greet");
+  const time = new Date().toISOString();
 
-    console.log(`hello ${name}`);
+  core.setOutput("time", time);
+  core.exportVariable("HELLO_TIME", time);
 
-    const time = new Date();
-    core.setOutput("time", time.toISOString());
+  core.startGroup("GitHub Context");
+  console.log(JSON.stringify(github.context, null, 2));
+  core.endGroup();
 
-    core.exportVariable("HELLO_TIME",time);
-
-    core.startGroup("Logging github context");
-    console.log(JSON.stringify(github.context, null, 2));
-    core.endGroup();
-
-} catch(error){
-    core.setFailed(error.message);
+} catch (error) {
+  core.setFailed(error.message);
 }
